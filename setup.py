d104b1210c8badb9daa2773090bb91e77d15afae
peymanras <peyman.rasouli1@gmail.com>
2020-06-02 23:45:52 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
 import os
 import requests
 import zipfile
-import shutil
 
 # Downloading YaDT C4.5 decision tree library
 url = 'http://pages.di.unipi.it/ruggieri/YaDT/YaDT2.2.0.zip'
 r = requests.get(url, allow_redirects=True)
 open('YaDT2.2.0.zip', 'wb').write(r.content)
 
-# Extracting required files from YaDT library
+# Extracting YaDT library in the root directory
 with zipfile.ZipFile('YaDT2.2.0.zip', 'r') as zip_ref:
     zip_ref.extractall('./')
 os.remove('YaDT2.2.0.zip')
 
-# Making YaDT files executable
-os.system("chmod 777 ./yadt/dTcmd")
-os.system("chmod 777 ./yadt/libtbb.so.2")
+# Making dTcmd file executable
+os.system("chmod +x ./yadt/dTcmd")
 
-# Moving YaDT files to the root folder
-shutil.copyfile('./yadt/dTcmd', './dTcmd')
-shutil.copyfile('./yadt/libtbb.so.2', './libtbb.so.2')
-
-# Removing .pyc files
+# Removing any .pyc file
 os.system("find . -name '*.pyc' -exec rm -f {} \;")
 
 # Creating a directory for the results
 os.makedirs("experiments")
 
 print("Setup is complete!")
+
